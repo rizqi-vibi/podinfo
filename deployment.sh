017ce05111f98eed133d6030c0db6534a9e16ba6
rizqifauzy <rizqifauzy10@gmail.com>
2022-12-29 13:31:03 +0700

    update

--- deployment.sh
+++ deployment.sh
@@ -7,20 +7,20 @@ limitCPU="$4"
 limitMemory="$5"
 reqCPU="$6"
 reqMemory="$7"
-dkiServiceName="$8"
+ServiceName="$8"
 rollbackTag="$9"
 yamlResources="kubernetes/${namespace}"
 yamlRollback="kubernetes/rollback"
 port="9898"
-imageUrl="dki-images-registry-vpc.ap-southeast-5.cr.aliyuncs.com/dki/${dkiServiceName}"
-if [[ $dkiServiceName == "customer-api" ]]; then
-  shortServiceName="idc.cust"
-elif [[ $dkiServiceName == "dkisprivyid-api" ]]; then
-  shortServiceName="dkisprivyid"
-else
-  getShortServiceName=$(echo $dkiServiceName | cut -d '-' -f1)
-  shortServiceName="idc.${getShortServiceName}"
-fi
+imageUrl="dki-images-registry-vpc.ap-southeast-5.cr.aliyuncs.com/dki/${ServiceName}"
+#if [[ $ServiceName == "customer-api" ]]; then
+#  shortServiceName="idc.cust"
+#elif [[ $ServiceName == "dkisprivyid-api" ]]; then
+#  shortServiceName="dkisprivyid"
+#else
+#  getShortServiceName=$(echo $ServiceName | cut -d '-' -f1)
+#  shortServiceName="idc.${getShortServiceName}"
+#fi
 
 
 if [[ ! -d $yamlResources ]]; then
@@ -33,7 +33,7 @@ fi
 cp -rf kubernetes/*.yaml $yamlResources
 
 function varsReplace {
-    sed -i "s|%{SERVICE_NAME}|${dkiServiceName}|g" *.yaml
+    sed -i "s|%{SERVICE_NAME}|${ServiceName}|g" *.yaml
     sed -i "s|%{POD_REPLICA}|${podReplica}|g" *.yaml
     sed -i "s|%{LIMIT_CPU}|${limitCPU}|g" *.yaml
     sed -i "s|%{LIMIT_MEMORY}|${limitMemory}|g" *.yaml
@@ -41,7 +41,7 @@ function varsReplace {
     sed -i "s|%{REQ_MEMORY}|${reqMemory}|g" *.yaml
     sed -i "s|%{PORT}|${port}|g" *.yaml
     sed -i "s|%{IMAGE_URL}|${imageUrl}|g" *.yaml
-    sed -i "s|%{SHORT_NAME}|${shortServiceName}|g" *.yaml
+    #sed -i "s|%{SHORT_NAME}|${shortServiceName}|g" *.yaml
     sed -i "s|%{NAMESPACE_KUBE}|${namespace}|g" *.yaml
 }
 
